eafc947d03dc3091f2cd456a0658bcc929b0f9ab
DaxWest <awest49@uwo.ca>
2024-11-05 16:19:00 -0500

    trying to get the output correct

--- WestDax_Project2.py
+++ WestDax_Project2.py
@@ -46,7 +46,7 @@ def solution_methods(v_initial, angle, t_step, method, air_res=0, gravity=g, mas
             r_step = r + (tau * ((v_step + v)/2))
             v, r = v_step, r_step
 
-        position = np.append(r)
+        position = np.append(v, r)
 
     return position
 v0 = 100 / 2.237 # 100 mph was given, have to convert mph to m/s
