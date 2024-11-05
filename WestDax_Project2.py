e5918f5074e9e9b95855b08dcf4de63cf99489cb
DaxWest <awest49@uwo.ca>
2024-11-05 15:03:14 -0500

    realized that r and gravity had to be in the form (x,y) and so i adjusted my variables accordingly

--- WestDax_Project2.py
+++ WestDax_Project2.py
@@ -12,16 +12,18 @@ h_initial = 1 #m
 
 #Part 1
 
-def solution_methods(v_initial, angle, t, method, air_res=0, gravity=g, mass=mass_ball, d=d_ball, rho=rho_air, h=h_initial, r=r_range):
+def solution_methods(v_initial, angle, t, method, air_res=0, gravity=g, mass=mass_ball, d=d_ball, rho=rho_air, h=h_initial):
 
     theta = angle
     tau = t
     vec = abs(v) #this, combined with rc make up the vector and value of v in the acceleration formula, dvdt, given
     rc = np.sqrt((v[0]**2) + (v[1])**2)
     A = (np.pi)*((d/2)**2)
+    acc = np.array([0, gravity])
 
-    a = -(gravity) - (vec * (air_res * rho * A * rc)/(2*mass))
+    a = -(acc) - (vec * (air_res * rho * A * rc)/(2*mass))
     v = np.array([v_initial * np.cos(theta), v_initial * np.sin(theta)]) #v has x and y components determined by trig
+    r = np.array([0, h_initial])
 
     if method == 'Euler':
         v_step = v + (tau * a)
