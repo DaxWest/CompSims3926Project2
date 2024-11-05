b4a3636faa447f57b1653aefc9f5e01ecd4c7f39
DaxWest <awest49@uwo.ca>
2024-11-05 14:18:47 -0500

    psudocoding function

--- WestDax_Project2.py
+++ WestDax_Project2.py
@@ -6,3 +6,13 @@ g = 9.81 #m/s2
 rho_air = 1.2 #kg/m3
 C_d = 0.35 #drag coefficient
 h_initial = 1 #m
+
+#Part 1
+
+def solution_methods(v, angle, t, method, air_res):
+    if method == 'Euler':
+        pass
+    elif method == 'Euler-Cromer':
+        pass
+    elif method == 'Midpoint':
+        pass
